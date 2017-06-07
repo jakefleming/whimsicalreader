29537664863ea89dbb58acec4d893dd0bd60b530
zachalbert <zachalbert@gmail.com>
2017-06-06 19:56:13 -0700

    Move styles and structure over from nthnth. Very broken.

--- js/_main.js
+++ js/_main.js
@@ -18,3 +18,25 @@ $(document).ready(function(){
 
 // Table of Contents title. Change text to localize
 $("#markdown-toc").prepend("<li><h6>{{ site.data.messages.locales[site.locale].overview }}</h6></li>");
+
+$(document).ready(function() {
+  /**
+   *
+   * Lazy loading via layzr.js
+   *
+  **/
+
+  import Layzr from 'layzr.js'
+
+  const instance = Layzr({
+    threshold: 100         // Load images that are 100% of the screen height away from the bottom of the viewport
+  })
+
+  document.addEventListener('DOMContentLoaded', function(event) {
+    instance
+      .update()           // track initial elements
+      .check()            // check initial elements
+      .handlers(true)     // bind scroll and resize handlers
+  });
+
+})
